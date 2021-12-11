cf236aeef01b4e8c58b8cb4dfd83c34fe762e5a8
Andres Peña Diaz <andres@Andress-MacBook-Pro.local>
2021-12-11 02:34:10 -0500

    up-to-date

--- script.py
+++ script.py
@@ -8,10 +8,10 @@ def execute():
                 cd /Users/andres/Documents/Uniandes/infracomp/infracomp-caso1
                 git remote update
                 var="$(git status -uno)"
-                if [[ $var =~ "nothing to commit" ]]; then
-                    echo "Up-to-date"
-                else
+                if [[ $var =~ "branch is behind" ]]; then
                     sudo systemctl restart manejador-comercios.service
+                else 
+                    echo Up-To-Date
                 fi'''
     os.system(bashCmd)
 execute()
